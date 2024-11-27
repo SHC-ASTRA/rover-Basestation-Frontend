a9e46ca48f2903f0d42d251409e85421ae3b03cb
Roald Schaum <178021507+10xCOBOL@users.noreply.github.com>
2024-11-27 01:46:34 +0000

    Cleaned up .css into app.html, show of style overrides

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,13 @@
         <h1>Test</h1>
         <p>This is pink</p>
         <h2>I hope this works properly</h2>
-</div>
+		<style>
+			h3
+			{
+				text-align: left;
+			}
+		</style>
+		<h3 color=rgb(0,255,0)>This should be on the left</h3>
+		<h1 color=rgb(255,255,255)>This should be white</h1>
 
-<script>
-    import '../styles/global.css';
-</script>
+</div>
