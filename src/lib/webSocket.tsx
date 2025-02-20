63f0b20d62e5ad1645639c33a16c43c9159b5ff1
ryleu <69326171+ryleu@users.noreply.github.com>
2025-02-20 02:34:47 +0000

    fix websocket implementation

--- src/lib/webSocket.tsx
+++ src/lib/webSocket.tsx
@@ -1,13 +1,17 @@
 import { useEffect, useState } from "react";
 import useWebSocket from "react-use-websocket";
-import { AllFeedbackData } from "./types";
+import { AutoFeedbackData, CoreFeedbackData, DigitFeedbackData, FaerieFeedbackData, SocketFeedbackData } from "./types";
 
 /**
  * Custom hook to setup the websocket connection and handle incoming messages
  */
 export const useWebSocketSetup = () => {
     // storing data in state to trigger re-renders
-    const [webSocketData, setWebSocketData] = useState<AllFeedbackData>(new AllFeedbackData());
+    const [autoFeedback, setAutoFeedback] = useState<null | AutoFeedbackData>(null);
+    const [coreFeedback, setCoreFeedback] = useState<null | CoreFeedbackData>(null);
+    const [digitFeedback, setDigitFeedback] = useState<null | DigitFeedbackData>(null);
+    const [faerieFeedback, setFaerieFeedback] = useState<null | FaerieFeedbackData>(null);
+    const [socketFeedback, setSocketFeedback] = useState<null | SocketFeedbackData>(null);
 
     // determine the websocket url based on the current url
     const host = window.location.host;
@@ -23,41 +27,37 @@ export const useWebSocketSetup = () => {
         if (lastMessage !== null) {
             // parse the data from the message
             const data = JSON.parse(lastMessage.data);
-            const newWsData = webSocketData;
 
             // put the data in the right place based on the type
             switch (data.type) {
                 case 'feedback:core/auto':
-                    newWsData.autoFeedback = data;
+                    setAutoFeedback(data);
                     break;
                 case 'feedback:core':
-                    newWsData.coreFeedback = data;
+                    setCoreFeedback(data);
                     break;
                 case 'feedback:core/digit':
-                    newWsData.digitFeedback = data;
+                    setDigitFeedback(data);
                     break;
                 case 'feedback:core/faerie':
-                    newWsData.faerieFeedback = data;
+                    setFaerieFeedback(data);
                     break;
                 case 'feedback:arm/socket':
-                    newWsData.socketFeedback = data;
+                    setSocketFeedback(data);
                     break;
             }
-
-            // update the state with the new data
-            setWebSocketData(newWsData);
         }
-    }, [lastMessage, webSocketData]);
+    }, [lastMessage]);
 
     // return the data and the function to send messages
     // each of the feedback types is returned separately so that components can choose which ones they want to use
     return {
         sendMessage,
         readyState,
-        autoFeedback: webSocketData.autoFeedback,
-        coreFeedback: webSocketData.coreFeedback,
-        digitFeedback: webSocketData.digitFeedback,
-        faerieFeedback: webSocketData.faerieFeedback,
-        socketFeedback: webSocketData.socketFeedback,
+        autoFeedback,
+        coreFeedback,
+        digitFeedback,
+        faerieFeedback,
+        socketFeedback,
     };
 }
