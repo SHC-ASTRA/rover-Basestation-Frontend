7983cd276a0e26a96a62657990e662bc67831183
ryleu <69326171+ryleu@users.noreply.github.com>
2025-02-20 02:23:58 +0000

    fix gamepad polling rate

--- src/lib/gamepad.tsx
+++ src/lib/gamepad.tsx
@@ -2,36 +2,48 @@ import { useEffect, useState } from "react";
 import GamepadContext from "./gamepadContext";
 import { GamepadState } from "./types";
 
-
+/**
+ * Provider for the GamepadContext. This component should wrap the root of the app.
+ * @returns 
+ */
 const GamepadProvider = ({ children }: { children: React.ReactNode }) => {
     const [gamepadState, setGamepadState] = useState(new GamepadState(null));
     let gamepad: Gamepad | null = null;
 
-    // update gamepad state every 100ms
+    // handle updating gamepad state
     useEffect(() => {
-        const interval = setInterval(() => {
+        let requestId: number;
+
+        // update gamepad state every frame (usually 60 times per second)
+        const updateGamepad = () => {
             setGamepadState(new GamepadState(gamepad));
-        }, 100);
-        return () => clearInterval(interval);
+            requestId = requestAnimationFrame(updateGamepad);
+        };
+        requestId = requestAnimationFrame(updateGamepad);
+
+        // when the component is unmounted, cancel the animation frame (prevents memory leaks)
+        return () => cancelAnimationFrame(requestId);
     }, [gamepad]);
 
-    function onGamepadConnected(event: GamepadEvent) {
+
+
+    window.addEventListener("gamepadconnected", ((event: GamepadEvent) => {
+        // when a gamepad connects, set it as the current gamepad if there isn't one already
         if (gamepad === null) {
             gamepad = event.gamepad;
         }
-    }
+    }));
 
-    function onGamepadDisconnected() {
+    window.addEventListener("gamepaddisconnected", (() => {
+        // when a gamepad disconnects, get the most recent gamepad that's still connected and make that main gamepad
         const first_gamepad = navigator.getGamepads()[0];
         if (first_gamepad) {
             gamepad = first_gamepad;
         } else {
+            // but if there isn't another gamepad, we just set it to null
             gamepad = null;
         }
-    }
-
-    window.addEventListener("gamepadconnected", onGamepadConnected);
-    window.addEventListener("gamepaddisconnected", onGamepadDisconnected);
+    }));
 
     return (
         <GamepadContext.Provider value={gamepadState}>
