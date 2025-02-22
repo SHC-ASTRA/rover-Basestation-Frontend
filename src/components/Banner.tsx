c3f93036c561ef9a9c18d05c1f7709ad25fa6d95
ryleu <69326171+ryleu@users.noreply.github.com>
2025-02-22 03:55:16 +0000

    what the fuck

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -8,7 +8,7 @@ function BannerButton(props: { path: string, img: string }) {
 	const currentLocation = useLocation();
 
 	return <button onClick={() => navigate(props.path)} className={`banner-button ${(currentLocation.pathname == props.path) && "banner-button-active"}`}>
-		<img src={props.img} className="button-image" />
+		<img src={props.img} className="banner-button-image" />
 	</button>
 }
 
@@ -20,20 +20,16 @@ export default function Banner() {
 			<div className="banner">
 				<button onClick={() => setIconAdr(iconAdr == '../src/assets/favicon.png' ? '../src/assets/serious.png' : '../src/assets/favicon.png')} className="lImg" >
 					<img src={iconAdr} alt="Astra Logo" className="logo" /></button>
-				<h1 className="banner-text">
-					UAH S.H.C ASTRA
-				</h1>
 
-				<BannerButton path="/" img={"../src/assets/banner_icons/manual.webp"} />
+				<BannerButton path="/" img={"../src/assets/banner_icons/autonomy.webp"} />
 				<BannerButton path={coreDrivingPath} img="../src/assets/banner_icons/rover.webp" />
 				<BannerButton path={armDigitPath} img="../src/assets/banner_icons/arm.webp" />
 				<BannerButton path={autonomyPath} img="../src/assets/banner_icons/autonomy.webp" />
 				<BannerButton path={debugPath} img="../src/assets/banner_icons/debug.webp" />
 
-				<BannerButton path={autonomyPath} img="../src/assets/banner_icons/autonomy.webp" />
-				<BannerButton path={debugPath} img="../src/assets/banner_icons/debug.webp" />
-
-				<img src="../src/assets/clucky.png" alt="Clucky!" className="rImg" />
+				<div className="banner-button">
+					<img className="banner-button-image" src="../src/assets/clucky.png" alt="Clucky!" />
+				</div>
 			</div>
 		</>
 	);
