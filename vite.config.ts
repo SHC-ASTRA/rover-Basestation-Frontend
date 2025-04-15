e7bc3990d733d05cab9f84eb5f9d8b859802f71e
ryleu <69326171+ryleu@users.noreply.github.com>
2025-04-15 00:46:09 +0000

    template things out

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [tailwindcss(), react(), tsconfigPaths()],
+  server: {
+    host: true,
+    port: 8090,
+    strictPort: true,
+  },
+  build: {
+    outDir: './dist',
+  }
 })
