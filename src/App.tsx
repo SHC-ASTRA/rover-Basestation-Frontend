0eb6bb2f28c0908ac712aeeb50d0bce6c6d02e33
Jack Schumacher <jackrschumacher@gmail.com>
2025-04-17 02:56:13 +0000

    Setting up sidebar for navigation. Need to fix page link and clean up + add button styles

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,34 @@ function App() {
 
 
   return (
+    // <header className="bg-black border-black dark:bg-gray-900 sticky top-0 z-50">
 
+    //   <div className="  max-w-screen-xl flex flex-column items-center justify-between mx-auto p-4 sticky top-0 z-50"></div>
+    //   <nav>
 
-    <div className="min-h-screen bg-black">
+    //   </nav>
+    //   <p className="text-amber-50"></p>
+    // </header>
 
+    <div className="flex">
+      <aside className="h-screen sticky top-0 bg-black">
+        <p className="text-white ">ASTRA Basestation</p>
+        //TODO: not sure that this forwards to the page
+        <a href="../pages/arm.tsx">
+          <button className='text-white'>This is a button</button>
+        </a>
 
+      </aside>
 
+      <main >
 
+      </main>
     </div>
+
+
+
+
+
 
 
   );
