c57930fae969784912e49077f0cca74eb3343587
jackrschumacher <jackrschumacher@gmail.com>
2025-04-19 19:16:27 +0000

    Start working on integrating icons and clean up

--- src/App.tsx
+++ src/App.tsx
@@ -12,13 +12,21 @@ function App() {
     //   </nav>
     //   <p className="text-amber-50"></p>
     // </header>
-
-    <div className="flex">
-      <aside className="h-screen sticky top-0 bg-black">
-        <p className="text-white ">ASTRA Basestation</p>
-        //TODO: not sure that this forwards to the page
-        <a href="../pages/arm.tsx">
-          <button className='text-white bg-overlay2 margin-auto'>This is a button</button>
+    <div className="flex ">
+      <aside className="h-screen sticky top-0 bg-black flex flex-col items-center space-y-2">
+        <p className="text-white">ASTRA Basestation</p>
+        <a href="../pages/Arm.tsx">
+          <button className='text-white bg-overlay2 rounded-md'>Arm Control</button>
+          {/* <svg className="fill-current w-4 h-4 mr-2" xmlns="../assets/banner_icons/arm.webp" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" /></svg> */}
+        </a>
+        <a href="../pages/Bio.tsx">
+          <button className='text-white bg-overlay2 rounded-md'>Biosensor Control</button>
+        </a>
+        <a href="../pages/Doom.tsx">
+          <button className='text-white bg-overlay2 rounded-md'>Doom</button>
+        </a>
+        <a href="App.tsx">
+          <button className='text-white bg-overlay2 rounded-md'>Home</button>
         </a>
 
       </aside>
