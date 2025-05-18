37c93ea37b181f2445b380eb3ac3b0310f45f4b8
ryleu <69326171+ryleu@users.noreply.github.com>
2025-05-18 21:50:28 +0000

    more bio updates

--- src/pages/arm/Bio.tsx
+++ src/pages/arm/Bio.tsx
@@ -1,18 +1,15 @@
-import ArmSocketFeedback from '../../components/arm/ArmSocketFeedback';
 import FaerieVoltages from '../../components/bio/FaerieVoltages';
 import BioControl from '../../components/bio/BioControl';
-import ArmControl from '../../components/arm/ArmControl';
 import { ControlContainer, FeedbackContainer } from '../../components/MainContainers';
 
 export default function ArmBioPage() {
 	return <>
 		<ControlContainer >
-			<ArmSocketFeedback />
 			<FaerieVoltages />
 			<BioControl />
 		</ControlContainer>
 		<FeedbackContainer>
-			<ArmControl />
+			<p>put something here eventually</p>
 		</FeedbackContainer>
 	</>
 }
