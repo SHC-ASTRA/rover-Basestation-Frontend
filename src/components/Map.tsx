a5dfe272a8b7a739a165949a85977bf5dc2e1b35
ryleu <69326171+ryleu@users.noreply.github.com>
2025-05-23 02:50:44 +0000

    maps work

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -2,16 +2,24 @@ import L, { LatLngTuple } from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import { useEffect, useState } from 'react';
 
-const coords: LatLngTuple = [38.39800, -110.7920];
+const coords: LatLngTuple = [38.4063, -110.7918];
 
 export default function MapComponent() {
     const [map, setMap] = useState<L.Map | null>(null);
 
     useEffect(() => {
-        const newMap = L.map('map', { scrollWheelZoom: false, zoomControl: false }).setView(coords, 13);
+        const newMap = L.map('map', { scrollWheelZoom: true, zoomControl: true }).setView(coords, 15);
 
-        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '© OpenStreetMap',
+        // L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        //     attribution: '© OpenStreetMap',
+        // }).addTo(newMap);
+        L.tileLayer.wms("/map", { // Base URL of your QGIS Server
+            layers: 'utah_terrain', // The 'Short name' of your layer from QGIS Project Properties > QGIS Server
+            format: 'image/png',
+            transparent: true,
+            version: '1.3.0', // WMS version
+            crs: L.CRS.EPSG4326, // Or EPSG3857 if your QGIS project is in that and you prefer it
+            attribution: 'Utah Terrain &copy; USGS | Served by QGIS Server'
         }).addTo(newMap);
 
         setMap(newMap);
