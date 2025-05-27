124f08b1632a40f7b094883ce91f8054e5fd06ef
ASTRA <astra@uah.edu>
2025-05-27 23:19:24 +0000

    precision -> fixed

--- src/components/bio/BioGimbal.tsx
+++ src/components/bio/BioGimbal.tsx
@@ -26,11 +26,11 @@ function Readouts() {
                 <div className="vertical-split">
                     <h3>
                         Latitude:&nbsp;
-                        <span>{coreFeedback ? coreFeedback.data.gps_lat.toPrecision(7) : "no data"}&nbsp;&deg;N</span>
+                        <span>{coreFeedback ? coreFeedback.data.gps_lat.toFixed(7) : "no data"}&nbsp;&deg;N</span>
                     </h3>
                     <h3>
                         Longitude:&nbsp;
-                        <span>{coreFeedback ? coreFeedback.data.gps_long.toPrecision(7) : "no data"}&nbsp;&deg;E</span>
+                        <span>{coreFeedback ? coreFeedback.data.gps_long.toFixed(7) : "no data"}&nbsp;&deg;E</span>
                     </h3>
                 </div>
             </div>
@@ -43,12 +43,12 @@ function Readouts() {
             <div className="vertical-split container">
                 <h2>Temperature</h2>
                 <h3>
-                    {bioFeedback ? bioFeedback.data.drill_temp.toPrecision(1) : "no data"}&nbsp;&deg;C
+                    {bioFeedback ? bioFeedback.data.drill_temp.toFixed(1) : "no data"}&nbsp;&deg;C
                 </h3>
             </div>
             <div className="vertical-split container">
                 <h2>Humidity</h2>
-                <h3>{bioFeedback ? bioFeedback.data.drill_humidity.toPrecision(1) : "no data"}&nbsp;%</h3>
+                <h3>{bioFeedback ? bioFeedback.data.drill_humidity.toFixed(1) : "no data"}&nbsp;%</h3>
             </div>
         </div>
     </>
