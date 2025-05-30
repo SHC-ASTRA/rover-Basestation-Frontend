fc225c8e20e3834d06b9561e4f01e4a97ead367c
Ifeatu Dunu <icd0007@uah.edu>
2025-05-30 12:15:27 +0000

    Websockets and Rendering working

--- src/components/arm-vis/ArmVisCanvas.tsx
+++ src/components/arm-vis/ArmVisCanvas.tsx
@@ -1,28 +1,28 @@
-import { useEffect, useRef, useState } from "react"
-import ArmVisDraw from "./ArmVisDraw"
+import React, { createContext, useEffect, useRef, useState } from "react"
+import ArmVisDraw, { ArmVisClass } from "./ArmVisDraw"
 import ArmVisRoverFeedback from "./ArmVisRoverFeedback";
+import ArmVisTest from "./ArmVisTest";
 
-const Id = "ArmVisId"
+
 
 export default function ArmVisCanvas() {
-    const vis_canvas = useRef(null)
+
+    const vis_canvas = useRef<HTMLCanvasElement>(null)
 
     const [contextInit, setContextInit] = useState(false);
 
     useEffect(() => {
         const cas = vis_canvas.current
+        setContextInit(true);
+        ArmVisClass.canvas = vis_canvas;
 
-        if (cas == null) {
-            setContextInit(false);
-        } else {
-            setContextInit(true);
-        }
-
-    }, []);
+    }, [vis_canvas]);
 
     return <div className="arm-vis-div">
-        <canvas id={Id} ref={vis_canvas} width={800} height={900}></canvas>
-        {contextInit ? <ArmVisDraw canvasCtx={vis_canvas.current.getContext("webgl2")} /> : null}
-        <ArmVisRoverFeedback />
+
+        <canvas ref={vis_canvas} width={800} height={900}>
+            {contextInit ? <ArmVisDraw canvasCtx={vis_canvas.current?.getContext("webgl2")} /> : null}</canvas>
     </div>
+
 }
+
